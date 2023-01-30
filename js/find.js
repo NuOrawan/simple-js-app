61d4eab936780a2600262613a7aa7da7969e902b
NuOrawan <117319337+NuOrawan@users.noreply.github.com>
2023-01-29 20:45:53 -0800

    Add pokemon name as a parameter to fetch its details from external API

--- js/find.js
+++ js/find.js
@@ -9,72 +9,62 @@
         let nameInput = $("#pokemonName");
             //Show error message if input is empty
         function showErrorMessage(input,message){
-            let container = input.parentElement; // The .input-wrapper
-
+            let messageContainer = input.parent(); // ie. .input-wrapper
             //Check and remove exisiting error message
-            let error = container.querySelector(".error-message");
+            let error = messageContainer.find(".error-message");
             if (error){
-                container.removeChild(error);
+                $('.error-message').remove();
             }
             // Now add the error if the message isn’t empty.
             if (message){
                 //Create div element with error-message class
-                let error = document.createElement("div");
-                error.classList.add("error-message");
-                error.innerText = message;
-                //Add error div to parent element ie. container
-                container.appendChild(error);
+                let error = $("<div class='error-message'></div>");
+                error.text(message);
+                //Add error div to parent element ie. messageContainer
+                messageContainer.append(error);
             }
-
         }
         //Validate name. Name should not be empty.
         function validateName(){
-            let value = nameInput.value;
+            
+            let value = nameInput.val();
             console.log ("nameInput: "+ value);
             if (!value){
                 showErrorMessage(nameInput, "Name is required.");
                 return false;
             }
-
             showErrorMessage(nameInput,null);
             return true;  
         }
-
         //Search pokemon in the array by name
         function findByName(name){
             //Check if name is String and not empty
             if(typeof name === 'string' && name.length > 0){
-                //Find a match and return object that match name
-                let result = pokemonList.filter(function(pokemon){
-                    return pokemon.name === name;
-                }); 
-                return result;
+                //Create variable for API url and get Pokemon by name
+                let apiUrl = "https://pokeapi.co/api/v2/pokemon/" + name;
+                console.log("findByName:" + name);
+                console.log("ApiUrl :" + apiUrl);
+                //Find a match by name and return its details ie. height and type
+                
+        
             } else {
                 console.log("Please try entering pokemon name again.");
             }
         }
         function validateForm(){
             let isValidName = validateName();
-            return isValidName;
+            if (isValidName){
+                findByName(nameInput.val());
+            }
         }
-        
-        
-        //form.addEventListener("submit", (e)=> {
-        //    e.preventDefault(); //Do not submit to the server
-        //    if(validateForm()){
-        //        alert("Success");
-        //    }
-        //} );
-        
-    $("form").submit(function(){
-    //alert("Submitted");
+    $("form").submit(function(e){
         e.preventDefault(); //Do not submit to the server
+        console.log("Name :" + nameInput);
         if(validateForm()){
-        alert("Success");
+            alert("Success");
+            findByName(nameInput);
         }
     });
-    
-    //nameInput.addEventListener("pokemonName", validateName);
-    nameInput.on("pokemonName", validateName);
+    nameInput.on("input", validateName);
    
 })();
