4fa574fdc40053e689c9f4185d1971a77500f1ed
NuOrawan <117319337+NuOrawan@users.noreply.github.com>
2023-02-19 15:15:36 -0800

    Remove some comments in script file

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,17 +2,13 @@
 /* global */
 
 const pokemonRepository = (function () {
-  // First, set pokemonList array to blank array. This array contains Pokémon data to display in the application.
+  // This array contains Pokémon data to display in the application.
   const pokemonList = [];
-  // Create variable for API url and get 20 pokemons
   const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=100';
-  // Declare modal container
   const modalContainer = document.querySelector('#modal-container');
-  // Loader
-  // let loaderElement = $(".loader");
   const loaderElement = document.querySelector('.loader');
 
-  // Add Pokemon objects to the array
+  // Add Pokemon objects to the array. Pokemon object contain name and url 
   function add (pokemon) {
     // Check if parameter is type of Object and not null
     if (
@@ -26,29 +22,22 @@ const pokemonRepository = (function () {
       console.log('Invalid input! Please try again.');
     }
   }
-
+  // Get all Pokemon from the list
   function getAll () {
     return pokemonList;
   }
 
-  // Add pokemon to List
+  // Create pokemon buttons and when a button is clicked show Pokemon's details. Input Pokemon is an object containing name
   function addListItem (pokemon) {
-    // Select Bootstrap list-group class
     // eslint-disable-next-line no-undef
     const pokemonListElement = $('.pokemon-list');
-
-    // Create button triggered modal element
     // eslint-disable-next-line no-undef
     const button = $(
       '<button class="pokemon-button btn btn-info float-md-left float-lg-left" data-target="#pokemonModal" data-target="modal">' +
         pokemon.name +
         '</button>'
     );
-
-    // Append button to div with class pokemon-list
     pokemonListElement.append(button);
-
-    // Add event listner to show Pokemon details when clicked
     button.on('click', function () {
       showDetails(pokemon);
     });
@@ -62,11 +51,11 @@ const pokemonRepository = (function () {
   // Hide loading message in div when page is done loading
   function hideLoadingMessage () {
     loaderElement.classList.add('loader-hidden');
-    // Remove loader
     // eslint-disable-next-line no-undef
     $('.loader').remove();
   }
 
+  // ??
   function loadList () {
     // Display loading message while Pokemons are being loaded.
     showLoadingMessage();
@@ -86,7 +75,6 @@ const pokemonRepository = (function () {
         hideLoadingMessage();
       })
       .catch(function (e) {
-        // Display error message in console
         console.error(e);
       });
   }
@@ -99,7 +87,6 @@ const pokemonRepository = (function () {
         return response.json();
       })
       .then(function (details) {
-        // Now we add the details to the item
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
         item.types = details.types;
@@ -109,7 +96,7 @@ const pokemonRepository = (function () {
       });
   }
 
-  // Show details of Pokemon in modal.
+  // Show details of Pokemon's name, height and type in modal.
   function showDetails (item) {
     pokemonRepository.loadDetails(item).then(function () {
       showModal(item);
@@ -123,7 +110,6 @@ const pokemonRepository = (function () {
     pokemon.types.forEach(function (obj) {
       const types = obj.type;
       const name = types.name;
-      // Add space between each type name and make them as one string
       typesElement = typesElement + name + ' ';
     });
     return typesElement;
@@ -144,27 +130,18 @@ const pokemonRepository = (function () {
     // Clear existing title and body in modal
     modalTitle.empty();
     modalBody.empty();
-
-    // Create name element for modal content
     // eslint-disable-next-line no-undef
     const nameElement = $('<h3>' + item.name + '</h3>');
-
-    // Create image element for Pokemon in modal content section
     // eslint-disable-next-line no-undef
     const imageElement = $('<img>');
     imageElement.addClass('.modal-img');
-
-    // Append imageElement with it's attributes
     imageElement.attr('src', item.imageUrl);
-
     imageElement.attr('width', '50%');
     imageElement.attr('height', '50%');
-    // Create height element
     // eslint-disable-next-line no-undef
     const heightElement = $(
       '<p class="h6">' + 'Height: ' + item.height + '</p>'
     );
-    // Create types element
     // eslint-disable-next-line no-undef
     const typesElement = $(
       '<p class="h6">' + 'Type: ' + showTypes(item) + '</p>'
@@ -201,27 +178,22 @@ pokemonRepository.loadList().then(function () {
   });
 });
 
-// Search form Validation
+// Search form validation
 (function () {
-  // const form = $('#findPokemon-form');
   // eslint-disable-next-line no-undef
   const nameInput = $('#pokemonName');
   // Show error message if input is empty
   function showErrorMessage (input, message) {
     const messageContainer = input.parent(); // ie. .input-wrapper
-    // Check and remove exisiting error message
     const error = messageContainer.find('.error-message');
     if (error) {
       // eslint-disable-next-line no-undef
       $('.error-message').remove();
     }
-    //  Now add the error if the message isn’t empty.
     if (message) {
-      // Create div element with error-message class
       // eslint-disable-next-line no-undef
       const error = $('<div class="break error-message"></div>');
       error.text(message);
-      // Add error div to parent element ie. messageContainer
       messageContainer.append(error);
     }
   }
@@ -237,7 +209,6 @@ pokemonRepository.loadList().then(function () {
   }
   // Search pokemon in the array by name
   function findByName (name) {
-    // Check if name is String and not empty
     if (typeof name === 'string' && name.length > 0) {
       // Create variable for API url and get Pokemon by name
       const apiUrl = 'https://pokeapi.co/api/v2/pokemon/' + name;
@@ -248,10 +219,9 @@ pokemonRepository.loadList().then(function () {
           return response.json();
         })
         .then(function (response) {
-          // Create an object that contain imageUrl before passing to showModal()
+          // Create an object, Pokemon that contains imageUrl to show in a modal
           const pokemon = response;
           pokemon.imageUrl = response.sprites.front_default;
-          // Show search result in a modal
           pokemonRepository.showModal(pokemon);
         })
         .catch(function (e) {
@@ -265,7 +235,6 @@ pokemonRepository.loadList().then(function () {
   function validateForm () {
     const isValidName = validateName();
     if (isValidName) {
-      // Convert input value to all lowercase
       findByName(nameInput.val().toLowerCase());
     }
   }
